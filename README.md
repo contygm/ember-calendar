b7cf06e2ca31eb296ed4d878a8870ec39f0a094c
Genevieve Conty <genevieve.conty@AV-MBP-7208.hitronhub.home>
2018-03-24 15:00:47 -0700

    built out massive weeks computed prop

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 What's this repo for? Learning and experimenting
 
 ## Tasks & Experiments
-- [ ] Build a basic calendar application **(in progress)**
+- [ ] Build a basic calendar application *(in progress)*
 - [ ] Upgrade to Ember 3.0
 - [ ] Move Calendar to Engine
 - [ ] Add a React engine
 - [ ] Calendar Extras
 
-**Note: I've got a ZenHub board going if you want to see my progress. Just click the tab at the top.**
+*Note: I've got a ZenHub board going if you want to see my progress. Just click the tab at the top.*
--- client/app/components/month-view.js
+++ client/app/components/month-view.js
@@ -1,4 +1,59 @@
 import Ember from 'ember';
+import DS from 'ember-data';
+
+// TODO: extract
+// this gets the model's value for each day
+function getValue(days, dayObj) {
+  const day = days.filter(day => day.get('date') === dayObj.date[0]);
+
+  if (day) {
+    dayObj.value = day.get('value');
+  }
+}
 
 export default Ember.Component.extend({
+  // NOTE: days comes from the component's instantiation
+  // NOTE: computed mechanics (callings days)
+  //
+  weeks: Ember.computed('days', function () {
+
+    return DS.PromiseArray.create({
+      promise: this.get('days').then(function (days) {
+        // first day of current month
+        //
+        const day = moment().date(1);
+        const currentMonth = day.month();
+
+        // if the day isn't the first day of the week (Sunday)
+        //
+        if (day.day() !== 0) {
+          // make it the first day of the week
+          //
+          day.day(0);
+        }
+
+        let weeks = [];
+
+        // TODO: refactor, feels a bit ineffecient, also why not put name of day in day obj
+        // https://momentjs.com/docs/#/parsing/
+        //
+        while (day.month() <= currentMonth) {
+          let week = [];
+          for (let i = 0; i < 7; i++) {
+            const dayObj = {
+              date: day.format('YYYY-MM-DD'),
+              num: day.format('D'),
+              isCurrentMonth: currentMonth === day.month(),
+            }
+            getValue(days, dayObj);
+            week.push(dayObj);
+            day.add(1, 'd');
+          }
+          weeks.push(week);
+        }
+
+        return ;
+      })
+    })
+  })
 });
