44eb8386784997b1b7601835f9740e705cc573a3
Genevieve Conty <genevieve.conty@AV-MBP-7208.hitronhub.home>
2018-03-24 18:46:53 -0700

    the streak counts now

--- client/app/controllers/calendar.js
+++ client/app/controllers/calendar.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  monthName: moment().format('MM YYYY'),
+  monthName: moment().format('MMMM YYYY'),
 });
--- client/app/models/calendar.js
+++ client/app/models/calendar.js
@@ -1,8 +1,24 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   notes: DS.attr('string', { defaultValue: ''}),
-  // days will have its own model
   days: DS.hasMany('day'),
+  count: 0,
+  // TODO: fix so loads streak initally
+  counter: Ember.observer('days.@each.value', function() {
+    this.get('days').then(days => {
+      let count = 0;
+      const day = moment();
+
+      let ds = days.filterBy('date', day.format('YYYY-MM-DD'));
+      while (ds.length === 1 && ds[0].get('value')) {
+        count++;
+        day.subtract(1, 'd');
+        ds = days.filterBy('date', day.format('YYYY-MM-DD'));
+      }
+      this.set('count', count)
+    })
+  })
 });
